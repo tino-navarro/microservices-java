4e9955babfbb241b20f9a3425519799d82e1ed74
Tino Markus Bueno Navarro <1138028@atitus.edu.br>
2026-05-22 21:51:43 -0300

    Incluido padrões de resistencia no currency-service também.

--- currency-service/src/main/java/br/edu/atitus/currencyservice/clients/BCBClient.java
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/clients/BCBClient.java
@@ -1,5 +1,6 @@
 package br.edu.atitus.currencyservice.clients;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface BCBClient {
     //Consulta data fixa 15/05/2026
     @GetMapping("/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?%40moeda='{moeda}'&%40dataCotacao='05-15-2026'&%24format=json")
+    @Retry(name = "bcb-client")
     BCBResponse getBCBCurrency(@PathVariable String moeda);
-
 }
--- currency-service/src/main/java/br/edu/atitus/currencyservice/controllers/CurrencyController.java
+++ currency-service/src/main/java/br/edu/atitus/currencyservice/controllers/CurrencyController.java
@@ -6,6 +6,7 @@ import br.edu.atitus.currencyservice.dtos.CurrencyDTO;
 import br.edu.atitus.currencyservice.entities.CurrencyEntity;
 import br.edu.atitus.currencyservice.repositories.CurrencyRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,10 +26,12 @@ public class CurrencyController {
 
     private final CurrencyRepository repository;
     private final BCBClient bcbClient;
+    private final CacheManager cacheManager;
 
-    public CurrencyController(CurrencyRepository repository, BCBClient bcbClient) {
+    public CurrencyController(CurrencyRepository repository, BCBClient bcbClient, CacheManager cacheManager) {
         this.repository = repository;
         this.bcbClient = bcbClient;
+        this.cacheManager = cacheManager;
     }
 
     @GetMapping("/convert")
@@ -41,35 +44,40 @@ public class CurrencyController {
         source = source.toUpperCase();
         target = target.toUpperCase();
 
-        String dataSource = "None";
-        CurrencyEntity currency = new CurrencyEntity();
-        currency.setSourceCurrency(source);
-        currency.setTargetCurrency(target);
-        if (source.equals(target)) {
-            currency.setConversionRate(1.0);
+        String dataSource = "Cache";
+        String nameCache = "currency";
+        CurrencyEntity currency = cacheManager.getCache(nameCache).get(source + target, CurrencyEntity.class);
+        if (currency == null) {
+            currency = new CurrencyEntity();
+            currency.setSourceCurrency(source);
+            currency.setTargetCurrency(target);
+            if (source.equals(target)) {
+                currency.setConversionRate(1.0);
 
-        } else {
-            try {
-                Double sourceRate = 1.0;
-                Double targetRate = 1.0;
-                if (!source.equals("BRL")) {
-                    BCBResponse response = bcbClient.getBCBCurrency(source);
-                    if (response.value().isEmpty()) throw new Exception("Currency not found for " + source);
-                    sourceRate = response.value().get(0).cotacaoVenda();
+            } else {
+                try {
+                    Double sourceRate = 1.0;
+                    Double targetRate = 1.0;
+                    if (!source.equals("BRL")) {
+                        BCBResponse response = bcbClient.getBCBCurrency(source);
+                        if (response.value().isEmpty()) throw new Exception("Currency not found for " + source);
+                        sourceRate = response.value().get(0).cotacaoVenda();
+                    }
+                    if (!target.equals("BRL")) {
+                        BCBResponse response = bcbClient.getBCBCurrency(target);
+                        if (response.value().isEmpty()) throw new Exception("Currency not found for " + target);
+                        targetRate = response.value().get(0).cotacaoVenda();
+                    }
+                    currency.setConversionRate(sourceRate / targetRate);
+                    dataSource = "Banco Central do Brasil";
+                } catch (Exception e) {
+                    currency = repository.
+                            findBySourceCurrencyAndTargetCurrency(source, target)
+                            .orElseThrow(() -> new Exception("Currency not found"));
+                    dataSource = "Local database";
                 }
-                if (!target.equals("BRL")) {
-                    BCBResponse response = bcbClient.getBCBCurrency(target);
-                    if (response.value().isEmpty()) throw new Exception("Currency not found for " + target);
-                    targetRate = response.value().get(0).cotacaoVenda();
-                }
-                currency.setConversionRate(sourceRate / targetRate);
-                dataSource = "Banco Central do Brasil";
-            } catch (Exception e) {
-                currency = repository.
-                        findBySourceCurrencyAndTargetCurrency(source, target)
-                        .orElseThrow(() -> new Exception("Currency not found"));
-                dataSource = "Local database";
             }
+            cacheManager.getCache(nameCache).put(source + target, currency);
         }
 
 
